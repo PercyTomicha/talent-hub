92866fb2c5daa83a80031d0b6aea9c145b05b628
Percy Tomicha <mspercywillans@gmail.com>
2025-12-14 12:30:48 -0400

    feat: integrate PreferenceCubit and implement PrefsListPage UI

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'di.dart';
 import 'core/router/app_router.dart';
 import 'features/users/presentation/cubit/user_cubit.dart' show UserCubit;
+import 'features/prefs/presentation/cubit/preference_cubit.dart'
+    show PreferenceCubit;
 
 class TalentHub extends StatelessWidget {
   const TalentHub({super.key});
@@ -12,6 +14,7 @@ class TalentHub extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => sl<UserCubit>()..fetchUsers()),
+        BlocProvider(create: (context) => sl<PreferenceCubit>()..loadPrefs()),
       ],
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
--- lib/features/prefs/presentation/pages/pref_list_page.dart
+++ lib/features/prefs/presentation/pages/pref_list_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubit/preference_cubit.dart';
+import '../cubit/preference_state.dart';
+import '../../../../commons/widgets/tile_user.dart';
+import '../../../../commons/widgets/banner_error.dart';
+import '../../../../commons/widgets/banner_loading.dart';
 
 class PrefsListPage extends StatelessWidget {
   const PrefsListPage({super.key});
@@ -8,13 +14,41 @@ class PrefsListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Talentos Guardados')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Center(child: Text('Lista de talentos guardados'))],
-        ),
+      body: BlocBuilder<PreferenceCubit, PreferenceState>(
+        builder: (context, state) {
+          if (state is PreferenceLoading) return const BannerLoading();
+
+          if (state is PreferenceSuccess) {
+            if (state.savedUsers.isEmpty) {
+              return const Center(
+                child: Text('No tienes talentos guardados aún.'),
+              );
+            }
+            return ListView.builder(
+              itemCount: state.savedUsers.length,
+              itemBuilder: (context, index) {
+                final user = state.savedUsers[index];
+                return TileUser(
+                  name: user.customName,
+                  onTap: () => context.push('/prefs/${user.id}'),
+                  trailing: TextButton(
+                    onPressed: () {
+                      context.read<PreferenceCubit>().removeUser(user.id);
+                    },
+                    child: const Text(
+                      'Eliminar',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          if (state is PreferenceError) return const BannerError();
+
+          return const SizedBox.shrink();
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.push('/prefs/new'),
