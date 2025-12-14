71f8244c007b441e6ffe89d358a4bc6a3b47f6f9
Percy Tomicha <mspercywillans@gmail.com>
2025-12-14 06:17:22 -0400

    feat: add user entity and model mapping

---
+++ lib/features/users/data/models/user_model.dart
@@ -0,0 +1,13 @@
+import '../../domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({required super.id, required super.name});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(id: json['id'], name: json['name']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name};
+  }
+}
