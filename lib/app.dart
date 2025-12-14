424a8ad98265db1858826375271c85594360ed31
Percy Tomicha <mspercywillans@gmail.com>
2025-12-14 08:18:19 -0400

    fix: centralize dependency injection with getIt service locator

--- lib/app.dart
+++ lib/app.dart
@@ -1,28 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'core/network/api_client.dart';
+import 'di.dart';
 import 'features/users/presentation/pages/user_page.dart';
 import 'features/users/presentation/cubit/user_cubit.dart' show UserCubit;
-import 'features/users/domain/usecases/get_users_usecase.dart';
-import 'features/users/data/datasources/user_remote_datasource.dart';
-import 'features/users/data/repositories/user_repository_impl.dart'
-    show UserRepositoryImpl;
 
 class TalentHub extends StatelessWidget {
   const TalentHub({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final apiClient = ApiClient();
-    final dataSource = UserRemoteDataSourceImpl(apiClient);
-    final repository = UserRepositoryImpl(dataSource);
-    final getUsersUseCase = GetUsersUseCase(repository);
-
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (context) => UserCubit(getUsersUseCase)..fetchUsers(),
-        ),
+        BlocProvider(create: (context) => sl<UserCubit>()..fetchUsers()),
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'di.dart';
 import 'app.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+
+  await init();
 
   runApp(const TalentHub());
 }
