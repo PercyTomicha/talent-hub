7c363353161e20eb7aed82f62e642b3ff77eb951
Percy Tomicha <mspercywillans@gmail.com>
2025-12-14 12:21:50 -0400

    feat: implement Drift database configuration and UserPrefs table

---
+++ lib/core/database/app_database.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+part 'app_database.g.dart';
+
+class UserPrefs extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get customName => text().withLength(min: 1, max: 50)();
+}
+
+@DriftDatabase(tables: [UserPrefs])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
