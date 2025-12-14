6759150245175c9c9ad86fa5031cda4656fd8cf7
Percy Tomicha <mspercywillans@gmail.com>
2025-12-14 12:31:41 -0400

    feat: implement PrefsFormPage and register dependencies for local storage

--- lib/di.dart
+++ lib/di.dart
@@ -1,10 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'core/network/api_client.dart';
+import 'core/database/app_database.dart';
 import 'features/users/presentation/cubit/user_cubit.dart';
+import 'features/prefs/domain/usecases/prefs_usecases.dart';
 import 'features/users/domain/usecases/get_users_usecase.dart';
+import 'features/prefs/presentation/cubit/preference_cubit.dart';
 import 'features/users/domain/repositories/user_repository.dart';
+import 'features/prefs/domain/repositories/prefs_repository.dart';
 import 'features/users/data/repositories/user_repository_impl.dart';
+import 'features/prefs/data/datasources/user_local_datasource.dart';
 import 'features/users/data/datasources/user_remote_datasource.dart';
+import 'features/prefs/data/repositories/prefs_repository_impl.dart';
 
 final sl = GetIt.instance;
 
@@ -20,4 +26,25 @@ Future<void> init() async {
   sl.registerLazySingleton(() => GetUsersUseCase(sl()));
 
   sl.registerFactory(() => UserCubit(sl()));
+
+  final database = AppDatabase();
+  sl.registerLazySingleton(() => database);
+
+  sl.registerLazySingleton<UserLocalDataSource>(
+    () => UserLocalDataSourceImpl(sl()),
+  );
+
+  sl.registerLazySingleton<PrefsRepository>(() => PrefsRepositoryImpl(sl()));
+
+  sl.registerLazySingleton(() => GetSavedUsersUseCase(sl()));
+  sl.registerLazySingleton(() => SaveUserUseCase(sl()));
+  sl.registerLazySingleton(() => DeleteUserUseCase(sl()));
+
+  sl.registerFactory(
+    () => PreferenceCubit(
+      getSavedUsersUseCase: sl(),
+      deleteUserUseCase: sl(),
+      saveUserUseCase: sl(),
+    ),
+  );
 }
--- lib/features/prefs/presentation/pages/pref_form_page.dart
+++ lib/features/prefs/presentation/pages/pref_form_page.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PrefsFormPage extends StatelessWidget {
+import '../cubit/preference_cubit.dart';
+
+class PrefsFormPage extends StatefulWidget {
   const PrefsFormPage({super.key});
+
+  @override
+  State<PrefsFormPage> createState() => _PrefsFormPageState();
+}
+
+class _PrefsFormPageState extends State<PrefsFormPage> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +28,42 @@ class PrefsFormPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Center(child: Text('Crear nuevo Talento'))],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nombre del Talento',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            TextField(controller: _nameController),
+            Spacer(),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final name = _nameController.text;
+                      if (name.isNotEmpty) {
+                        context.read<PreferenceCubit>().saveNewUser(name);
+                        context.pop();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('El nombre no puede estar vacío'),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Guardar'),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 56),
+          ],
         ),
       ),
     );
